564f52395f6091771e8393f09531d2a33de117df
hxero <hxeropro@gmail.com>
2026-06-07 18:29:04 +0700

    dksoadkp

--- curleave/curleave.cpp
+++ curleave/curleave.cpp
@@ -64,13 +64,16 @@ APICALL EXPORT std::string PLUGIN_API_VERSION() { return HYPRLAND_API_VERSION; }
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     PHANDLE = handle;
 
-    if (__hyprland_api_get_hash() != __hyprland_api_get_client_hash()) {
+    const std::string HASH        = __hyprland_api_get_hash();
+    const std::string CLIENT_HASH = __hyprland_api_get_client_hash();
+
+    if (HASH != CLIENT_HASH) {
         HyprlandAPI::addNotification(PHANDLE, "[curleave] Version mismatch",
                                      CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
-        throw std::runtime_error("[curleave] Version mismatch");
+        throw std::runtime_error("[bpp] Version mismatch");
     }
 
-    static auto g_moveListener = Event::bus()->m_events.input.mouse.move.listen(
+    g_moveListener = Event::bus()->m_events.input.mouse.move.listen(
         [](const Vector2D &pos, Event::SCallbackInfo &info) {
             onMouseMove(pos, info);
         });
